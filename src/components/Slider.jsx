3034c87fe40a8edb7fab3c352e0fb0ac76cad71b
Henrique Matheus <109422748+HenriqueMatheusDev@users.noreply.github.com>
2023-02-16 04:43:16 -0300

    Add files via upload

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -5,8 +5,8 @@ const Container = styled.div`
     width: 100%;
     height: 100vh;
     display: flex;
-    background-color: coral;
     position: relative;
+    overflow: hidden;
 `
 
 const Arrow = styled.div`
@@ -23,15 +23,94 @@ const Arrow = styled.div`
     left: ${props=> props.direction === "left" && "10px"};
     right: ${props=> props.direction === "right" && "10px"};
     margin: auto;
+    cursor: pointer;
+    opacity: 0.5;
+    Z-index: 2;
+`
+const Wrapper = styled.div`
+    height: 100%;
+    display: flex;
+    transform: translateX(0vw)
+`
+const Slide = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    background-color: #${props=>props.bg};
+`
+const ImgContainer = styled.div`
+    height: 100%;
+    flex: 1;
+`
+const Image = styled.img`
+    flex: 80%;
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+const Title = styled.h1`
+    font-size:70px;
+`
+const Desc = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
 `
 
 const Slider = () => {
+
+    const handleClick = (direction) => {
+
+    }
+
   return (
     <Container>
-      <Arrow direction="left">
+      <Arrow direction="left" onClick={()=>handleClick("left")}>
         <ArrowLeftOutlined/>
       </Arrow>
-      <Arrow direction="right">
+      <Wrapper>
+        <Slide bg="f5fafd">
+        <ImgContainer>
+         <Image src="https://images.pexels.com/photos/1092198/pexels-photo-1092198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        </ImgContainer>
+        <InfoContainer>
+            <Title>SUMMER SAL</Title>
+            <Desc>DONT´T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+            <Button>SHOW NOW</Button>
+        </InfoContainer>
+        </Slide>
+        <Slide bg="fcf1ed">
+        <ImgContainer>
+         <Image src="https://images.pexels.com/photos/1092198/pexels-photo-1092198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        </ImgContainer>
+        <InfoContainer>
+            <Title>WINTER SAL</Title>
+            <Desc>DONT´T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+            <Button>SHOW NOW</Button>
+        </InfoContainer>
+        </Slide>
+        <Slide bg="fbf0f4">
+        <ImgContainer>
+         <Image src="https://images.pexels.com/photos/1092198/pexels-photo-1092198.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
+        </ImgContainer>
+        <InfoContainer>
+            <Title>POPULAR SAL</Title>
+            <Desc>DONT´T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS</Desc>
+            <Button>SHOW NOW</Button>
+        </InfoContainer>
+        </Slide>
+      </Wrapper>
+      <Arrow direction="right" onClick={()=>handleClick("right")}>
         <ArrowRightOutlined/>
       </Arrow>
     </Container>
