9990559ba0f89cc6700ccc4503e7f87fcd0873e3
Henrique Matheus <109422748+HenriqueMatheusDev@users.noreply.github.com>
2023-02-16 01:27:43 -0300

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from "@material-ui/icons"
+import { Badge } from "@material-ui/core"
+import { Search, ShoppingCartOutlined } from "@material-ui/icons"
 import React from 'react'
 import styled from 'styled-components'
 
@@ -33,9 +34,22 @@ const Input = styled.input`
 
 const Center = styled.div`
     flex: 1;
+    text-align: center;
 `
+const Logo = styled.h1`
+    font-weight: bold;
+`
+
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
 `
 
 const Navbar = () => {
@@ -46,11 +60,20 @@ const Navbar = () => {
             <Language>EN</Language>
             <SearchContainer>
                 <Input/> 
-                <Search/>
+                <Search style={{color:"gray", fontSize:16}}/>
             </SearchContainer>
            </Left>
-           <Center>center</Center>
-           <Right>right</Right>
+           <Center><Logo>LAMA.</Logo></Center>
+           <Right>
+            
+            <MenuItem>REGISTER</MenuItem>
+            <MenuItem>SIGN IN</MenuItem>
+            <MenuItem>
+            <Badge badgeContent={4} color="primary">
+            <ShoppingCartOutlined />
+            </Badge>
+            </MenuItem>
+           </Right>
         </Wrapper>
     </Container>
   )
